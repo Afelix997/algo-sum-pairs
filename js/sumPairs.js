1b0c3a09a65088c5432589f14f36e63bd6024460
Luis Manzo <manzosk3@gmail.com>
2022-05-26 19:10:39 -0500

    pair programming with Angel Felix

--- js/sumPairs.js
+++ js/sumPairs.js
@@ -1,3 +1,19 @@
-exports.sumPairs = function() {
-
+function sumPairs(arr, target) {
+  let res = [];
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+    for(let k = i + 1; k < arr.length; k++) {
+      if(arr[i] + arr[k] === target) {
+       res.push([arr[i], arr[k]]);
+      }
+    }
+  }
+  if(res.length === 0) {
+    return('unable to find pairs');
+  }
+  return res;
 };
+
+console.log(sumPairs([1,2,3,4,5], 9));
+console.log(sumPairs([1,2,3,4,5], 7));
+console.log(sumPairs([3, 1, 5, 8, 2], 27));
