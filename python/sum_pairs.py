9fce736014497638b7fb58a10574eea07ba9d1f8
afelix997 <afelix2013@gmail.com>
2022-05-29 18:06:39 -0700

    answer

--- python/sum_pairs.py
+++ python/sum_pairs.py
@@ -1,1 +1,15 @@
-def sum_pairs():
+def sum_pairs(arr , target):
+    no_pairs='unable to find pairs'
+    res= []
+    sum=0
+    for index1,i in enumerate(arr):
+            for index2,k in enumerate(arr):
+                if arr[index1] >=arr[index2]:
+                        continue
+                if arr[index1]+arr[index2]== target:
+                    res.append( [arr[index1],arr[index2]])
+                   
+    if len(res) == 0:
+        return no_pairs
+    return res
+
